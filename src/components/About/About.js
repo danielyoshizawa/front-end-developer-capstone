52f297e468847c2a01a6e71a15fb184b9851f855
daniel.yoshizawa <yoshidanielcwb@gmail.com>
2023-03-20 16:01:36 -0300

    Adding about section

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,6 +1,25 @@
+import './Style.css'
+import chefA from './Images/restaurantchefB.jpg'
+import chefB from './Images/MarioandAdrianA.jpg'
+
 function About() {
   return(
-    <section>About</section>
+    <section id="about" className="about">
+      <div className="info">
+        <div>
+          <h2>Little Lemon</h2>
+          <h3>Chicago</h3>
+        </div>
+        <p>
+          We are a family owned Mediterranean restaurant,
+          focused on traditional recipes served with a modern twist.
+        </p>
+      </div>
+      <div className="image">
+        <img id="chefA" src={chefA} alt="Our chefs" />
+        <img id="chefB" src={chefB} alt="Our chefs" />
+      </div>
+    </section>
   )
 }
 
